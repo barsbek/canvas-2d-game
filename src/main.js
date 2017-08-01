a3f310a3e05ca3e6109e6219cac536df7cfcf932
toshiro <mak.bekmamat@gmail.com>
2017-08-01 09:56:40 +0500

    make main file more object oriented

--- src/main.js
+++ src/main.js
@@ -1,33 +1,68 @@
 document.addEventListener('DOMContentLoaded', init);
 
 const Control = require('./elements/control');
+const Ball = require('./elements/ball');
+
 
 function init() {
   const canvas = document.querySelector('#plot');
-  const ctx = canvas.getContext('2d');
-  [canvas.width, canvas.height] = [window.innerWidth, window.innerHeight];
+  const game = new Game(canvas);
+  game.start();
+}
 
-  const control = new Control(ctx, canvas);
-  control.draw();
-
-  document.addEventListener('resize', () => {
-    [canvas.width, canvas.height] = [window.innerWidth, window.innerHeight];
-    controls.draw();
-  });
-
-  document.addEventListener('keydown', e => {
-    if(e.keyCode == 'A'.charCodeAt()) control.moveLeft();
-    else if(e.keyCode == 'D'.charCodeAt()) control.moveRight();
-  });
-
-  document.addEventListener('keyup', e => {
-    control.stop();
-  })
-
-  function redraw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    control.draw();
-    requestAnimationFrame(redraw);
+class Game {
+  constructor(canvas) {
+    if(canvas.tagName == 'canvas'.toUpperCase()) {
+      this.canvas = canvas;
+    } else {
+      throw new Error('Canvas element is not found');
+    }
+    this.ctx = canvas.getContext('2d');
   }
-  requestAnimationFrame(redraw);
+
+  start() {
+    this.initCanvas();
+    this.initElements();
+    this.initListeners();
+    requestAnimationFrame(this.redraw.bind(this));
+  }
+
+  initCanvas() {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+  }
+
+  initElements() {
+    this.control = new Control(this.ctx, this.canvas);
+    this.ball = new Ball(this.ctx, this.canvas);
+  }
+
+  drawElements() {
+    this.control.draw();
+    this.ball.draw();
+  }
+
+  redraw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ball.move();
+    this.drawElements();
+    requestAnimationFrame(this.redraw.bind(this));
+  }
+
+  initListeners() {
+    window.addEventListener('resize', () => {
+      this.initCanvas();
+      this.initElements();
+      this.drawElements();
+    });
+
+    document.addEventListener('keydown', e => {
+      if(e.keyCode == 'A'.charCodeAt()) this.control.moveLeft();
+      else if(e.keyCode == 'D'.charCodeAt()) this.control.moveRight();
+    });
+
+    document.addEventListener('keyup', e => {
+      this.control.stop();
+    });
+  }
 }
