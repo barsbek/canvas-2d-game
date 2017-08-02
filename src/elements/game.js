87f5f070c2c95a7d8da91566eddbfbe50edd3de0
toshiro <mak.bekmamat@gmail.com>
2017-08-02 13:25:06 +0500

    show results

--- src/elements/game.js
+++ src/elements/game.js
@@ -10,8 +10,13 @@ class Game {
       throw new Error('Canvas element is not found');
     }
     this.ctx = canvas.getContext('2d');
-    this.bricksNumber = {x: 8, y: 3};
+    this.bricksNumber = {x: 2, y: 1};
     this.bricks = [];
+    this.score = {
+      value: 0,
+      position: {x: 10, y: 10},
+      size: 0.05*this.canvas.height
+    };
   }
 
   start() {
@@ -40,6 +45,9 @@ class Game {
     const margin = {x: 0.2*area.x, y: 0.4*area.y};
     const size = {width: area.x - margin.x, height: area.y - margin.y};
 
+    this.score.position = {x: margin.x*0.5, y: margin.y*0.5};
+    margin.y = margin.y + this.score.size;
+
     for(let x=0; x<this.bricksNumber.x; x++) {
       for(let y=0; y<this.bricksNumber.y; y++) {
         let position = {x: x*area.x + margin.x/2, y: y*area.y+margin.y};
@@ -61,22 +69,46 @@ class Game {
     }
   }
 
+  drawResult(result) {
+    let color = 'green';
+    if(!result) result = `Score: ${this.score.value}`;
+    else color = 'red';
+
+    this.ctx.font = `${this.score.size}px Arial`;
+    this.ctx.fillStyle = color;
+    this.ctx.textBaseline = "top";
+    this.ctx.fillText(result, this.score.position.x, this.score.position.y);
+  }
+
   removeBrick() {
     let index = this.ball.hitBrick(this.bricks);
-
     if(index || index === 0) {
       this.bricks.splice(index, 1);
+      this.score.value++;
     }
+  }
+
+  win() {
+    const bricksNumber = this.bricksNumber.x*this.bricksNumber.y;
+    if(this.score.value == bricksNumber) return true;
+    else return false;
   }
 
   redraw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-    let result = this.ball.move();
+    let result, moved = this.ball.move();
     this.removeBrick();
     this.drawElements();
-    if(result)
+
+    if(this.win()) {
+      result = 'You win';
+    } else if(moved) {
       requestAnimationFrame(this.redraw.bind(this));
+    } else {
+      result = 'You lost';
+    }
+    this.drawResult(result);
   }
 
   initListeners() {
